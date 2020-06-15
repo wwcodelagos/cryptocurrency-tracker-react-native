da9fd03ba6a30e47f9dec347ef17af81694e2f0b
uchemukolo <uche.mukolo@jjj.work>
2020-06-15 17:56:48 +0100

    add card component

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import { Card} from 'react-native-elements'
+import CardComponent from './Card';
 
 export default function HomeScreen({navigation}) {
     return (
       <View style={styles.container}>
-          <Button
+      <Card containerStyle={styles.card} title='Total Value' titleStyle={{marginTop: 20, color: '#fff', fontWeight: 'normal', fontSize: 14}} dividerStyle={{backgroundColor: 'pink' }}>
+      <Text style={{borderRadius: 0, marginLeft: 110, fontSize: 28, marginTop: 0, color: '#fff', fontWeight: 'bold' }}>$ 524.00</Text>
+      </Card>
+      <View>
+      <Text style={styles.listHeader}>List of Coins</Text>
+      </View>
+  
+      <CardComponent />
+      
+        
+      <Button
         title="Go to Details Screen"
         onPress={() => navigation.navigate('Details')}
       />
@@ -15,9 +27,29 @@ export default function HomeScreen({navigation}) {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#ebebeb',
-      alignItems: 'center',
-      justifyContent: 'center',
-
+      backgroundColor: '#F9F8F9',
     },
+    card: {
+      justifyContent: 'flex-start',
+      color: '#ffffff',
+        width: '90%',
+        height: 150,
+        marginTop: 20,
+        alignSelf: 'center',
+        backgroundColor: 'pink',
+        borderRadius: 5,
+        borderWidth: 1,
+        shadowColor: '#000',
+        shadowRadius: 6.5,
+        elevation: 12,
+        shadowOpacity: 0.1,
+        shadowOffset: {
+          width: 0,
+          height:6,
+        }
+      },
+      listHeader: {
+        marginLeft: 150,
+        marginTop: 20
+      }
   });
