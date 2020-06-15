a3336886a698f1cb563ad37ab3def0c31e078899
Tobi <ttobi4@gmail.com>
2020-06-15 18:28:31 +0100

    error fix

--- AppNavigator.js
+++ AppNavigator.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './components/HomeScreen';
-import DetailsScreen from './components/DetailsScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
 
 const Stack = createStackNavigator();
 
