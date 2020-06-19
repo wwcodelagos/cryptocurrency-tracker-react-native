4f25681db6670e222bb80f8b592c3dc43941fb21
Tobi Taiwo <ttobi4@gmail.com>
2020-06-19 12:09:20 +0100

    Fetch Funtion for API Request (#4) (#6)
    * add fetch fucntion for api request
    
    * Delete .env
    
    Co-authored-by: Martins Aloba <aloba.martins@gmail.com>

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './AppNavigator';
+import getRequest from './lib/request'
 
 
 function App() {
+  useEffect(() => {
+    getRequest()
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <AppNavigator />
   );
