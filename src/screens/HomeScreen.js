1e7cc8e3f8cc9a089f097a22b57352d92190d0ec
Deborah Balogun <balogundeborah28@gmail.com>
2020-06-23 21:10:18 +0100

    fusing-live-data and scrollview component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, Button, Text, ScrollView } from 'react-native';
 import { Card} from 'react-native-elements'
 import CardComponent from '../components/Card';
 import getRequest from '../../lib/request';
@@ -61,37 +61,39 @@ const currencies = [
   },
  ]
 
-export default class HomeScreen extends React.Component {
-  componentDidMount() {
-    getRequest().then(data => console.log(data))
+export default function HomeScreen({navigation}) {
+  const [currencies, setCurrencies] = useState([])
+  const goToDetails = () => {
+    navigation.navigate('Details');
   }
+  useEffect(() => {
+    const getApi = async () => {
+      setCurrencies(await getRequest());
+    }
+    getApi();
+  }, [setCurrencies])
+console.log('>>>>>', currencies)
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+    <Card containerStyle={styles.card} title='Total Value' titleStyle={{marginTop: 20, color: '#fff', fontWeight: 'normal', fontSize: 14}} dividerStyle={{backgroundColor: 'pink' }}>
+    <Text style={{borderRadius: 0, marginLeft: 110, fontSize: 28, marginTop: 0, color: '#fff', fontWeight: 'bold' }}>$ 524.00</Text>
+    </Card>
+    <View>
+    <Text style={styles.listHeader}>List of Coins</Text>
+      {currencies && currencies.map((currency, index) => {
+        return  <CardComponent onPress={goToDetails} key={index} currency={currency}/>
+      })}
+    </View>
+    <Button
+      title="Go to Details Screen"
+      onPress={() => navigation.navigate('Details')}
+    />
+    </ScrollView>
+    </View>
 
-  goToDetails = () => {
-    this.props.navigation.navigate('Details');
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-      <Card containerStyle={styles.card} title='Total Value' titleStyle={{marginTop: 20, color: '#fff', fontWeight: 'normal', fontSize: 14}} dividerStyle={{backgroundColor: 'pink' }}>
-      <Text style={{borderRadius: 0, marginLeft: 110, fontSize: 28, marginTop: 0, color: '#fff', fontWeight: 'bold' }}>$ 524.00</Text>
-      </Card>
-      <View>
-      <Text style={styles.listHeader}>List of Coins</Text>
-        {currencies.map((currency, index) => {
-          return  <CardComponent onPress={this.goToDetails} key={index} currency={currency}/>
-        })}
-      </View>
-        
-      <Button
-        title="Go to Details Screen"
-        onPress={this.goToDetails}
-      />
-      </View>
-    );
-  }
+  );
 }
-  
   const styles = StyleSheet.create({
     container: {
       flex: 1,
