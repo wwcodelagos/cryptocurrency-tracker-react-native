29dac7aabd6b6e329cd8e4af4e3686e9e47d49a3
Deborah Balogun <balogundeborah28@gmail.com>
2020-06-24 21:26:40 +0100

    fuse-live-data  with scrollview

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,62 +4,6 @@ import { Card} from 'react-native-elements'
 import CardComponent from '../components/Card';
 import getRequest from '../../lib/request';
 
-const currencies = [
-  {
-    id: 1,
-     name: 'Bitcoin',
-     symbol: 'BTC',
-     avatar: 'https://static.coincap.io/assets/icons/btc@2x.png',
-     price: '$ 9,2945.00',
-     change: 0.08,
-     volumeUsd24Hr: '$ 123,333,679'
-  },
-  {
-    id: 2,
-      name: 'Ethereum',
-      symbol: 'ETC',
-      avatar: 'https://static.coincap.io/assets/icons/etc@2x.png',
-      price: '$ 2,2945.00',
-      change: 0.05,
-      volumeUsd24Hr: '$ 3,333,679'
-   },
-   {
-    id: 3,
-    name: 'Tether',
-    symbol: 'USDT',
-    avatar: 'https://static.coincap.io/assets/icons/usdt@2x.png',
-    price: '$ 9,2945.00',
-    change: 0.08,
-    volumeUsd24Hr: '$ 123,333,679'
- },
- {
-  id: 4,
-     name: 'XRP',
-     symbol: 'XRP',
-     avatar: 'https://static.coincap.io/assets/icons/xrp@2x.png',
-     price: '$ 2,2945.00',
-     change: 0.05,
-     volumeUsd24Hr: '$ 3,333,679'
-  },
-  {
-    id: 5,
-    name: 'Bitcoin Cash',
-    symbol: 'BCH',
-    avatar: 'https://static.coincap.io/assets/icons/bch@2x.png',
-    price: '$ 9,2945.00',
-    change: 0.08,
-    volumeUsd24Hr: '$ 123,333,679'
- },
- {
-  id: 6,
-     name: 'Bitcoin SV',
-     symbol: 'BSV',
-     avatar: 'https://static.coincap.io/assets/icons/bsv@2x.png',
-     price: '$ 2,2945.00',
-     change: 0.05,
-     volumeUsd24Hr: '$ 3,333,679'
-  },
- ]
 
 export default function HomeScreen({navigation}) {
   const [currencies, setCurrencies] = useState([])
@@ -72,7 +16,7 @@ export default function HomeScreen({navigation}) {
     }
     getApi();
   }, [setCurrencies])
-console.log('>>>>>', currencies)
+    
   return (
     <View style={styles.container}>
       <ScrollView>
